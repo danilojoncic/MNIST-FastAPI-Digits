2a325dfd806ce209e53fbea196fc126dd1733b99
danilojoncic <djoncic14122rn@raf.rs>
2025-05-30 22:27:57 +0200

    fast api base upload

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+from msilib.schema import File
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+
+@app.get("/health-check")
+async def health_check():
+    return {"status": "ok"}
+
+
+@app.get("/model-details")
+async def model_details():
+    return {"model": "My Model"}
+
+@app.post("/predict")
+async def predict():
+    return "Working on it!"
