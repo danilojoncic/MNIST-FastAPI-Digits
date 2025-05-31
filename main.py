27a66af8b2526cf49dd3231c563707e381ed612b
danilojoncic <djoncic14122rn@raf.rs>
2025-06-01 00:50:11 +0200

    index.html with canvas added

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-
-
-
+@app.get("/paint-screen", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/health-check")
 async def health_check():
